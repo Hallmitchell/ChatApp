d6612e60ee4f5b31aa2554ace00b6bb61e352258
Hallmitchell <mitchjhall2002@gmail.com>
2025-08-07 23:56:35 -0500

    header update

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -5,7 +5,6 @@
     import {currentUser} from './pocketbase';
 </script>
 <div  style="display: flex; justify-content:space-between;">
-    <h1>ChatAPP</h1>
     {#if $currentUser}
         <p>signed in as @{$currentUser.userName}</p>
     {/if}
